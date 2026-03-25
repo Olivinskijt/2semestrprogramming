9c46b6a2ed81ec7ea89c216d96a4dddae7b00467
Olivinskijt <st9149201@stud.duikt.edu.ua>
2026-03-25 17:17:16 +0100

    task2

--- ModuleControl1/ModuleControl1/Task1.cs
+++ ModuleControl1/ModuleControl1/Task1.cs
@@ -1,52 +1,52 @@
-﻿using System;
-using System.IO;
+﻿//using System;
+//using System.IO;
 
-namespace FileIO.WorkingWithTextFiles
-{
-    internal class FileInfoTextOperations
-    {
-        delegate string TextOperation(string s);
+//namespace ModuleControl1
+//{
+//    internal class Task1
+//    {
+//        delegate string TextOperation(string s);
 
-        static void Main()
-        {
-            string folder = @"C:\Users\Olivinskiy\source\repos\2semestrprogramming\ModuleControl1\ModuleControl1\";
-            string inputName = Path.Combine(folder, "textPD23.txt");
-            string outputName = Path.Combine(folder, "resultPD23.txt");
+//        static void Main()
+//        {
+//            string folder = @"C:\Users\Olivinskiy\source\repos\2semestrprogramming\ModuleControl1\ModuleControl1\";
+//            string inputName = Path.Combine(folder, "textPD23.txt");
+//            string outputName = Path.Combine(folder, "resultPD23.txt");
 
-            FileInfo inputFile = new FileInfo(inputName);
-            FileInfo outputFile = new FileInfo(outputName);
+//            FileInfo inputFile = new FileInfo(inputName);
+//            FileInfo outputFile = new FileInfo(outputName);
 
-            using (var writer = new StreamWriter(outputFile.FullName, false))
-            {
-                writer.Write("");
-            }
+//            using (var writer = new StreamWriter(outputFile.FullName, false))
+//            {
+//                writer.Write("");
+//            }
 
-            ProcessFile(inputFile, outputFile, ToUpperCase);
-            ProcessFile(inputFile, outputFile, CountChars);
-            ProcessFile(inputFile, outputFile, CountWords);
+//            ProcessFile(inputFile, outputFile, ToUpperCase);
+//            ProcessFile(inputFile, outputFile, CountChars);
+//            ProcessFile(inputFile, outputFile, CountWords);
 
-            Console.WriteLine("Файл збережено:");
-            Console.WriteLine(outputFile.FullName);
-        }
+//            Console.WriteLine("Файл збережено:");
+//            Console.WriteLine(outputFile.FullName);
+//        }
 
-        static void ProcessFile(FileInfo input, FileInfo output, TextOperation op)
-        {
-            string[] lines = File.ReadAllLines(input.FullName);
+//        static void ProcessFile(FileInfo input, FileInfo output, TextOperation op)
+//        {
+//            string[] lines = File.ReadAllLines(input.FullName);
 
-            using (var writer = new StreamWriter(output.FullName, true))
-            {
-                foreach (string line in lines)
-                {
-                    writer.WriteLine(op(line));
-                }
-                writer.WriteLine("-----");
-            }
-        }
+//            using (var writer = new StreamWriter(output.FullName, true))
+//            {
+//                foreach (string line in lines)
+//                {
+//                    writer.WriteLine(op(line));
+//                }
+//                writer.WriteLine("-----");
+//            }
+//        }
 
-        static string ToUpperCase(string s) => s.ToUpper();
+//        static string ToUpperCase(string s) => s.ToUpper();
 
-        static string CountChars(string s) => s.Length.ToString();
+//        static string CountChars(string s) => s.Length.ToString();
 
-        static string CountWords(string s) => s.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length.ToString();
-    }
-}
+//        static string CountWords(string s) => s.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length.ToString();
+//    }
+//}
---
+++ ModuleControl1/ModuleControl1/Task2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace ModuleControl1
+{
+    class MessagePublisher
+    {
+        public event Action<string> MessageSent;
+
+        public void Send(string message)
+        {
+            MessageSent?.Invoke(message);
+        }
+    }
+
+    class FileLogger
+    {
+        private string logFile;
+
+        public FileLogger(string filePath, MessagePublisher publisher)
+        {
+            logFile = filePath;
+            publisher.MessageSent += LogMessage;
+        }
+
+        private void LogMessage(string message)
+        {
+            using var writer = new StreamWriter(logFile, true);
+            writer.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {message}");
+        }
+    }
+
+    internal class Task2
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            string folder = @"C:\Users\Olivinskiy\source\repos\2semestrprogramming\ModuleControl1\ModuleControl1\";
+            string logFile = Path.Combine(folder, "logPD2X.txt");
+
+            var publisher = new MessagePublisher();
+            var logger = new FileLogger(logFile, publisher);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Введіть текст: ");
+                string message = Console.ReadLine();
+                publisher.Send(message);
+            }
+
+            Console.WriteLine($"Лог збережено у {logFile}");
+        }
+    }
+}
